aaf844bbe1bb0c500c66f14d6f63b60a1c97dfc6
narifumi7 <is0399rk@ed.ritsumei.ac.jp>
2020-01-15 18:42:22 +0900

    update

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-from sotsuken import Translate,WrongWord,WrongWord2
+from AllCode import Translate,WrongWord,WrongWord2,Wiki
 #翻訳のテスト
-l = ["百万遍では立て看が撤去されました。","嫌いです。しかし、大好きです。"]
-print(Translate(l))
-print("文字を入力してください")
-#WrongWordのテスト
-v = input()
-print(WrongWord(v))
-#WrongWord2のテスト
-s=["微分"]
+l = ["微分"]
+m = ["嫌いです。しかし、大好きです。"]
+n = ["微分","嫌いです。しかし、大好きです。"]
+print(Translate(l,m))
+#print("文字を入力してください")
+##WrongWordのテスト
+#v = input()
+#print(WrongWord(v))
+##WrongWord2のテスト
+s=["風林火山"]
 l = ["形態素解析","関数","数学","積分"]
-print(WrongWord2(s,l))
+#print(WrongWord2(s,l))
+#Wiki("微分")
